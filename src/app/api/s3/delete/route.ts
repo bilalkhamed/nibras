f5860546bbd57d52638c9cb0ae9e9d3cbeeefc67
Bilal <bilalkhamed1@gmail.com>
2026-01-06 17:08:33 +0300

    feat: add authentication and authorization for file deletion

--- src/app/api/s3/delete/route.ts
+++ src/app/api/s3/delete/route.ts
@@ -1,8 +1,20 @@
+import getAuthSession from '@/lib/server/auth-session';
 import { S3 } from '@/lib/server/s3-client';
 import { DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { Role } from '@prisma/client';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function DELETE(req: NextRequest) {
+  const session = await getAuthSession();
+
+  if (!session) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  if (session.role !== Role.admin) {
+    return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
+  }
+
   try {
     const body = await req.json();
     const { key } = body;
