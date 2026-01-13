2f5fac759d09cb780cb7cf37432db60ef603b79d
Bilal <bilalkhamed1@gmail.com>
2026-01-14 01:38:18 +0300

    refactor: remove redundant auth check and use cached getAllUsers

--- src/app/dashboard/(admin)/users/users-table-section.tsx
+++ src/app/dashboard/(admin)/users/users-table-section.tsx
@@ -1,36 +1,9 @@
-import { cookies } from 'next/headers';
 import { UsersTable } from './table';
-import { ACCESS_TOKEN_COOKIE, verifyAccessToken } from '@/lib/server/tokens';
-import prisma from '@/lib/server/prisma';
 import { CountryCode } from '@/types/types';
+import { getAllUsers } from '@/lib/server/users';
 
 export default async function UsersTableSection() {
-  //dummy wait
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const cookie = (await cookies()).get(ACCESS_TOKEN_COOKIE)?.value;
-
-  const accessToken = await verifyAccessToken(cookie || '');
-
-  if (!accessToken || accessToken.role !== 'admin') {
-    return <div>Unauthorized</div>;
-  }
-  const users = await prisma.user.findMany({
-    select: {
-      id: true,
-      firstName: true,
-      middleName: true,
-      lastName: true,
-      email: true,
-      role: true,
-      birthYear: true,
-      country: true,
-      status: true,
-      createdAt: true,
-      updatedAt: true,
-      phone: true,
-      cohort: { select: { id: true, name: true } },
-    },
-  });
+  const users = await getAllUsers();
 
   return (
     <UsersTable
