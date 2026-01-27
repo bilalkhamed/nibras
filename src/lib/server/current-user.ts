6a202111c3881397bfc2099c859ab8b850a808c2
Bilal <bilalkhamed1@gmail.com>
2026-01-27 19:51:55 +0300

    refactor: replace direct database access with service function in getCurrentUser

--- src/lib/server/current-user.ts
+++ src/lib/server/current-user.ts
@@ -1,30 +1,19 @@
-import prisma from './prisma';
-import { AccessTokenPayload, CountryCode, User } from '@/types/types';
+import { AccessTokenPayload, CountryCode } from '@/types/types';
 import getAuthSession from './auth-session';
+import { getUserById } from '@/features/users/service';
+import { UserDTO } from '@/features/users/types';
 
 export async function getCurrentUser(
-  authSession?: AccessTokenPayload
-): Promise<User | null> {
+  authSession?: AccessTokenPayload,
+): Promise<UserDTO | null> {
   const auth = authSession ?? (await getAuthSession());
   if (!auth) return null;
 
-  const user = await prisma.user.findUnique({
-    where: { id: auth.userId },
-    select: {
-      id: true,
-      email: true,
-      role: true,
-      firstName: true,
-      status: true,
-      country: true,
-      middleName: true,
-      lastName: true,
-      birthYear: true,
-      createdAt: true,
-      updatedAt: true,
-      phone: true,
-    },
-  });
+  const resposne = await getUserById(auth.userId);
+
+  if (!resposne.success) return null;
+
+  const user = resposne.data;
 
   if (!user) return null;
   return { ...user, country: user.country as CountryCode };
