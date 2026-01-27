e4499d5e9c2dd4a7399d0d9c26c5bc1ab05e703b
Bilal <bilalkhamed1@gmail.com>
2026-01-27 23:15:19 +0300

    refactor: remove testing upload page component

--- src/app/dashboard/(admin)/upload/page.tsx
+++
@@ -1,9 +0,0 @@
-import { FileUploader } from '@/components/forms/file-uploader';
-
-export default function UploadPage() {
-  return (
-    <div className="p-4 flex items-center justify-center ">
-      <FileUploader />
-    </div>
-  );
-}
