f1299e049f93476548f81c830854a88505459017
Bilal <bilalkhamed1@gmail.com>
2026-02-01 22:41:33 +0300

    refactor: move users page to (staff) and change (admin) to be accessible by admins only

--- src/app/dashboard/(staff)/users/layout.tsx
+++ src/app/dashboard/(staff)/users/layout.tsx
@@ -1,10 +1,12 @@
 import { AuthGuard } from '@/components/auth/auth-gaurd';
 import { ADMIN_ROLE, COHORT_MANAGER_ROLE } from '@/types/types';
 
-export default async function AdminDashboardLayout({
+export default function UsersLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AuthGuard roles={[ADMIN_ROLE]}>{children}</AuthGuard>;
+  return (
+    <AuthGuard roles={[ADMIN_ROLE, COHORT_MANAGER_ROLE]}>{children}</AuthGuard>
+  );
 }
