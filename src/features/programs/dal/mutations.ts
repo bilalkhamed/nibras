fe94f4fc7f2e49628028a7fd382281ed0005738f
Bilal <bilalkhamed1@gmail.com>
2026-04-06 23:29:31 +0300

    fix: use transaction context instead of global prisma client for deleting weeks in updateCalendarWeeks function

--- src/features/programs/dal/mutations.ts
+++ src/features/programs/dal/mutations.ts
@@ -77,7 +77,7 @@ export async function updateCalendarWeeks(
         }),
       );
 
-      await prisma.week.deleteMany({
+      await tx.week.deleteMany({
         where: {
           AND: [
             {
