5149f6f6ec6a78ca6110794b39d636946798a6f3
Bilal <bilalkhamed1@gmail.com>
2026-06-24 20:33:27 +0300

    feat: add student status display in group students table

--- src/features/groups/components/shared/group-students-table.tsx
+++ src/features/groups/components/shared/group-students-table.tsx
@@ -6,22 +6,19 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { formatDate } from '@/lib/shared/utils';
+import { cn, formatDate } from '@/lib/shared/utils';
 import { StudentActions } from '../admin/student-actions';
 import { ADMIN_ROLE, COHORT_MANAGER_ROLE } from '@/types/types';
-
-export interface GroupStudent {
-  student: {
-    id: string;
-    firstName: string;
-    middleName: string | null;
-    lastName: string;
-  };
-  joinedAt: Date;
-}
+import { GroupStudentInfoDTO } from '../../types';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import labels from '@/lib/labels.json';
 
 interface GroupStudentsTableProps {
-  groupStudents: GroupStudent[];
+  groupStudents: GroupStudentInfoDTO[];
   groupId: string;
   userRole: string;
 }
@@ -50,7 +47,26 @@ export function GroupStudentsTable({
               className="border-b border-border hover:bg-muted/50"
             >
               <TableCell className="text-right font-medium">
-                {student.firstName} {student.middleName} {student.lastName}
+                <div className="flex items-center gap-2">
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <span
+                        className={cn(
+                          'inline-block h-2 w-2 rounded-full transition-all duration-300 opacity-70 hover:opacity-100',
+                          student.status === 'active'
+                            ? 'bg-emerald-500 hover:bg-emerald-400 hover:shadow-[0_0_8px_#10b981] hover:scale-110'
+                            : 'bg-secondary hover:bg-secondary/80 hover:shadow-[0_0_8px_rgba(156,163,175,0.8)] hover:scale-110',
+                        )}
+                      />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      {labels.dashboard.users[student.status]}
+                    </TooltipContent>
+                  </Tooltip>
+                  <span>
+                    {student.firstName} {student.middleName} {student.lastName}
+                  </span>
+                </div>
               </TableCell>
 
               <TableCell className="text-right text-sm">
